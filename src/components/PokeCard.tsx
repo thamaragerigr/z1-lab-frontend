9cd316fa911114e3546873b7ce4ad7e288ad78b8
thamaragerigr <thamaragerigr@gmail.com>
2020-07-13 22:02:27 +0200

    added styled components

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,4 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Card = styled.div`
+    margin-right: 1rem;
+    margin-bottom: 1rem;
+    padding: 1.9rem 1rem;
+    border-radius: 1rem;
+    background-color: #F2F2F2;
+    width: 156px;
+    height: 120px;
+`;
+
+const CardTitle = styled.h2`
+    color: #4F4F4F;
+    font-weight: bold;
+    font-size: 1rem;
+    text-transform: capitalize;
+`;
 
 interface Props {
   name: string;
@@ -10,10 +28,12 @@ interface Props {
 const PokeCard:React.FC<Props> = (props) => {
     return(
      <div>
-       PokeCard of {props.name} <br/>
-       PokeCard of {props.number} <br/>
-       PokeCard of {props.abilities} <br/>
-       PokeCard of {props.image} <br/>
+        <Card>
+          <CardTitle>{props.name}</CardTitle>
+          #00{props.number} <br/>
+          PokeCard of {props.abilities} <br/>
+          <img src={props.image} alt={props.name} /> <br/>
+        </Card>
      </div>
     )
 }
--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -3,42 +3,86 @@ import { useQuery } from 'react-query'
 import styled from 'styled-components'
 import PokeCard from './PokeCard'
 
+const Wrapper = styled.div`
+    padding: 1rem 1.6rem;
+    font-family: Helvetica;
+    margin: auto;
+`;
+
+const CardWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
 const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  font-family: Helvetica;
+    font-weight: normal;
+    padding: 1.5rem 0rem;
+    font-size: 30px;
 `;
 
 const Pokedex:React.FC = () => {
     const [pokemons, setPokemons] = useState<any[]>([])
-    const [isLoading, setIsLoading] = useState(true)
+    // const [isLoading, setIsLoading] = useState(true)
 
-    useQuery('repoData', () => 
+    const { isLoading, error } = useQuery('pokemonData', () => 
       fetch('https://pokeapi.co/api/v2/pokemon/?limit=10')
       .then(res => res.json())
-      .then(res => {
-        setPokemons(res.results)
-        setIsLoading(false)
-      }).catch(error => console.log(error))
+      .then(data => {
+        setPokemons(data.results)
+        // setIsLoading(false)
+      })
     )
+
+    if (isLoading) return (
+       <div> 
+         'Loading...' 
+       </div>
+    )
+
+    if (error) console.log(error)
     
     return(
-        <div>
+        <Wrapper>
         <Title>
             Pokedex
         </Title>
-        {isLoading && "Loading..."}
-        {pokemons.map(pokemon => (
-            <PokeCard 
-            key={pokemon.name}
-            name={pokemon.name} 
-            number={2} 
-            abilities={["namehere", "woah"]} 
-            image={pokemon.url} 
-          />
-        ))}
-    </div>
+        <CardWrapper>
+            {pokemons.map(pokemon => (
+                <PokeCard 
+                key={pokemon.name}
+                name={pokemon.name} 
+                number={2} 
+                abilities={["namehere", "woah"]} 
+                image={pokemon.url} 
+            />
+            ))}
+        </CardWrapper>
+    </Wrapper>
     )
 }
 
 export default Pokedex
+
+// useQuery('repoData', () => 
+// fetch('https://pokeapi.co/api/v2/pokemon/?limit=10')
+// .then(res => res.json())
+// .then(data => {
+//   setResult(
+//       data.results.map((i: any) => {
+//           fetch(i.url)
+//           .then((res) => res.json())
+//           .then((allpokemons) => arr.push(allpokemons))
+//           setPokemons(arr)
+//           console.log(pokemons)
+//           setIsLoading(false)
+//       })
+//   )
+//   setIsLoading(false)
+// })
+// )
+
+// if (isLoading) return (
+//  <div> 
+//    'Loading...' 
+//  </div>
+// )
