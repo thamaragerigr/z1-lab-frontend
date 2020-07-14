7ee5572a56e6eaef365889d0b691ed6f3ad37c60
thamaragerigr <thamaragerigr@gmail.com>
2020-07-14 14:22:55 +0200

    added new syntax for style components

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,47 +1,49 @@
 import React from "react";
 import styled from "styled-components";
-import { Props } from '../types/types'
 import PokeballSvg from "../assets/PokeCard/pokeballCard.svg";
+import { Props } from "../types/types";
 
 const PokeCard: React.FC<Props> = (props) => {
   return (
     <div>
-      <Card>
-        <CardNumber>#00{props.number}</CardNumber>
-        <CardTitle>{props.name}</CardTitle>
-        <CardContent>
+      <$Card>
+        <$CardNumber>#00{props.number}</$CardNumber>
+        <$CardTitle>{props.name}</$CardTitle>
+        <$CardContent>
           <div>
             {props.types.map((i) => (
-              <PokemonType key={i.type.name}>{i.type.name}</PokemonType>
+              <$PokemonType key={i.type.name}>{i.type.name}</$PokemonType>
             ))}
           </div>
           <img src={props.image} alt={props.name} />
-          <img src={PokeballSvg} alt="Pokeball"/>
-        </CardContent>
-      </Card>
+        </$CardContent>
+      </$Card>
     </div>
   );
 };
 
-const Card = styled.div`
+const $Card = styled.div`
   margin-right: 0.5rem;
   margin-bottom: 1rem;
   width: 9.5rem;
   height: 7.5rem;
   padding: 1rem;
   border-radius: 1rem;
+  background-image: url(${PokeballSvg});
+  background-repeat: no-repeat;
+  background-position: right bottom;
   background-color: #f2f2f2;
   position: relative;
 `;
 
-const CardTitle = styled.h2`
+const $CardTitle = styled.h2`
   color: #4f4f4f;
   font-weight: bold;
   font-size: 0.9rem;
   text-transform: capitalize;
 `;
 
-const CardNumber = styled.p`
+const $CardNumber = styled.p`
   color: #828282;
   font-weight: bold;
   font-size: 0.9rem;
@@ -51,13 +53,13 @@ const CardNumber = styled.p`
   right: 1rem;
 `;
 
-const CardContent = styled.div`
+const $CardContent = styled.div`
   display: flex;
   justify-content: space-between;
   margin-top: 0;
 `;
 
-const PokemonType = styled.p`
+const $PokemonType = styled.p`
   color: #4f4f4f;
   font-weight: bolder;
   font-size: 6px;
--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -37,13 +37,13 @@ const Pokedex: React.FC = () => {
   if (error) console.log(error);
 
   return (
-    <Wrapper>
-      <Title>Pokedex</Title>
-      <Pokeball>
+    <$Wrapper>
+      <$Title>Pokedex</$Title>
+      <$Pokeball>
         <img src={PokeballSvg} alt="Pokeball" />
-      </Pokeball>
+      </$Pokeball>
       {load && "Loading..."}
-      <CardWrapper>
+      <$CardWrapper>
         {pokemons.map((pokemon, index) => (
           <PokeCard
             key={pokemon.name}
@@ -53,32 +53,33 @@ const Pokedex: React.FC = () => {
             image={pokemon.sprites.front_default}
           />
         ))}
-      </CardWrapper>
-    </Wrapper>
+      </$CardWrapper>
+    </$Wrapper>
   );
 };
 
-const Wrapper = styled.div`
+const $Wrapper = styled.div`
   font-family: Helvetica;
   padding: 1rem;
   margin: 0 auto;
-  max-width: 24rem;
+  max-width: 24.5rem;
   position: relative;
 `;
 
-const CardWrapper = styled.div`
+const $CardWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
 `;
 
-const Title = styled.h1`
+const $Title = styled.h1`
   font-weight: normal;
   padding: 1.5rem 0rem;
   font-size: 30px;
+  color: #303943;
 `;
 
-const Pokeball = styled.div`
+const $Pokeball = styled.div`
   position: absolute;
   right: 0;
   top: 0;
